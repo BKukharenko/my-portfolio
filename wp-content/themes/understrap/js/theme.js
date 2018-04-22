c02e718368271065d267e8ef5402fc058d7fd898
BKukharenko <bohdankukharenko@gmail.com>
2018-04-22 23:40:25 +0300

    Add script

--- wp-content/themes/understrap/js/theme.js
+++ wp-content/themes/understrap/js/theme.js
@@ -3926,3 +3926,27 @@ Object.defineProperty(exports, '__esModule', { value: true });
 		}, false );
 	}
 })();
+
+jQuery(function($) {
+
+// handle links with @href started with '#' only
+    $(document).on('click', 'a[href^="#"]', function (e) {
+        // target element id
+        var id = $(this).attr('href');
+
+        // target element
+        var $id = $(id);
+        if ($id.length === 0) {
+            return;
+        }
+
+        // prevent standard hash navigation (avoid blinking in IE)
+        e.preventDefault();
+
+        // top position relative to the document
+        var pos = $id.offset().top;
+
+        // animated top scrolling
+        $('body, html').animate({scrollTop: pos}, 1000);
+    });
+});
